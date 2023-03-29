ea4addc154524a346b13474c3ebcaa8193b3f307
etienne-baroth <etienne.baroth@laplateforme.io>
2023-03-29 11:08:04 +0200

    job02 et 03 installation

---
+++ jour03/job02/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+
+    $('.btn').on('click', function() {
+      const mix = $('mix').find(img)
+      img.sort()
+    })
+
+  })
--- jour03/job03/index.php
+++ jour03/job03/index.php
@@ -11,18 +11,6 @@
 
 <body>
 
-<button class="btn">Mélanger</button>
-<div class="container" style="width: 800px; height: 500px; border: 1px solid black"></div>
-
-  <div class="mix">
-    <img src="arc1.png" id="arc1">
-    <img src="arc2.png" id="arc2">
-    <img src="arc3.png" id="arc3">
-    <img src="arc4.png" id="arc4">
-    <img src="arc5.png" id="arc5">
-    <img src="arc6.png" id="arc6">
-  </div>
-
   <script src="script.js"></script>
 </body>
 
